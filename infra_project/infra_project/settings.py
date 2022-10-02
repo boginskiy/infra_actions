b1441b790f151d3af0656df28cef47d1a1f5ccc1
boginskiy <1.boginskiy@mail.ru>
2022-10-02 13:39:32 +0300

    ref 2

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '62.84.121.234'
+    '51.250.105.182',
+    '127.0.0.1'
 ]
 
 # Application definition
